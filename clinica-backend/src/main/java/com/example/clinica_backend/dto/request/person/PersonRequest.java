9a57f2747b564360157b88ea3277d982885e3176
Jorge Luis Silvestre Ayala <jorgeluisasilvetsre2024@gmail.com>
2026-04-12 17:34:01 -0500

    Cambios nuevos

--- clinica-backend/src/main/java/com/example/clinica_backend/dto/request/person/PersonRequest.java
+++ clinica-backend/src/main/java/com/example/clinica_backend/dto/request/person/PersonRequest.java
@@ -1,22 +1,17 @@
-<<<<<<<< HEAD:clinica-backend/src/main/java/com/example/clinica_backend/model/dto/request/person/PersonRequest.java
-package com.example.clinica_backend.model.dto.request.person;
-========
 package com.example.clinica_backend.dto.request.person;
 
 import java.time.LocalDate;
->>>>>>>> dev:clinica-backend/src/main/java/com/example/clinica_backend/dto/request/person/PersonRequest.java
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDate;
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+
 public class PersonRequest {
 
     private String documentType;
@@ -28,5 +23,5 @@ public class PersonRequest {
     private String phoneNumber;
     private String address;
     private String email;
-
+    
 }
--- clinica-backend/src/main/java/com/example/clinica_backend/model/entity/Doctor.java
+++ clinica-backend/src/main/java/com/example/clinica_backend/model/entity/Doctor.java
@@ -1,7 +1,16 @@
 package com.example.clinica_backend.model.entity;
-import jakarta.persistence.*;
-import lombok.*;
 import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @AllArgsConstructor
@@ -21,6 +30,5 @@ public class Doctor {
     @MapsId // Usa el mismo ID de Person
     @JoinColumn(name = "id") // FK hacia persons(id)
     private Person person;
-
 
 }
